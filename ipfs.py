bf8577efb03214423db2b019db075d635ce5d9dc
CarlZhou <wuxi8black@gmail.com>
2025-06-27 18:48:30 +0000

    fix with pinata

--- ipfs.py
+++ ipfs.py
@@ -21,7 +21,7 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid, content_type="json"):
     assert isinstance(cid, str), "get_from_ipfs accepts a cid in the form of a string"
 
-    url = f"https://ipfs.io/ipfs/{cid}"
+    url = f"https://gateway.pinata.cloud/ipfs/{cid}"
     response = requests.get(url)
     response.raise_for_status()
 
