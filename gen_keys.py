8ae9bfe8629bf0816762164a49354ed67651a038
CarlZhou <wuxi8black@gmail.com>
2025-07-11 19:43:04 +0000

    fix

--- gen_keys.py
+++ gen_keys.py
@@ -22,14 +22,10 @@ def sign_message(challenge, filename="secret_key.txt"):
     # TODO recover your account information for your private key and sign the given challenge
     # Use the code from the signatures assignment to sign the given challenge
     
-    signed_message = eth_account.Account.sign_message(message, key)
-    eth_addr = eth_account.Account.from_key(key).address
-
-    recovered_addr = eth_account.Account.recover_message(
-        message,
-        signature=signed_message.signature
-    )
-    assert recovered_addr == eth_addr, f"Failed to sign message properly"
+    private_key = key[0].strip()
+    account = w3.eth.account.from_key(private_key)
+    signed_message = account.sign_message(message)
+    eth_addr = account.address
 
     assert eth_account.Account.recover_message(message,signature=signed_message.signature.hex()) == eth_addr, f"Failed to sign message properly"
 
