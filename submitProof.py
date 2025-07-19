b8e50ab56bb0c2df231072dbe03047e6e9b26a81
CarlZhou <wuxi8black@gmail.com>
2025-07-19 18:10:05 +0000

    fix build

--- submitProof.py
+++ submitProof.py
@@ -178,8 +178,9 @@ def send_signed_msg(proof, random_leaf):
 
     # TODO YOUR CODE HERE
     contract = w3.eth.contract(address=address, abi=abi)  
-    proof_hex = [Web3.toHex(p) for p in proof]  
-    leaf_hex = Web3.toHex(random_leaf)  
+    proof_hex = ['0x' + p.hex() for p in proof]
+    leaf_hex  = '0x' + random_leaf.hex()
+    
     try:  
         gas_estimate = contract.functions.claimPrime(leaf_hex, proof_hex).estimateGas({'from': acct.address})  
     except Exception:  
